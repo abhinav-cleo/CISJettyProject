3009b2d2b7ec9adcf9be6a1b0d999fc3d880799b
abhinav-cleo <agupta@cleo.com>
2017-01-02 18:48:01 +0530

    Updated the Service to fetch data from the Backend after everty 500 ms

--- src/main/ng2Client/src/app/dashboard-data.service.ts
+++ src/main/ng2Client/src/app/dashboard-data.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import {Http} from "@angular/http";
 import "rxjs/add/operator/map";
+import {Observable} from "rxjs";
 @Injectable()
 export class DashboardDataService {
 
   constructor(private http: Http) { }
   readData(param) {
-    return this.http
-      .get(param)
-      .map(response => {
-        return response
-      });
+      return Observable.interval(5000)
+          .flatMap(() => this.http.get(param))
+          .map(response => response);
   }
 }
