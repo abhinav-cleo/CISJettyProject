14fe81e30ce03e838597e33fc8b711f17c6698f6
abhinav-cleo <agupta@cleo.com>
2017-01-03 09:55:32 +0530

    Configured Bootstrap

--- src/main/ng2Client/src/app/app.module.ts
+++ src/main/ng2Client/src/app/app.module.ts
@@ -1,24 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {AppComponent} from "./app.component";
 import {Dashboard} from "./dashboard";
 import {DashboardDataService} from "./dashboard-data.service";
 import {HelloWorld} from "./helloWorld.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Dashboard,
-    HelloWorld
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [DashboardDataService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        Dashboard,
+        HelloWorld
+    ],
+    imports: [
+        NgbModule.forRoot(),
+        BrowserModule,
+        FormsModule,
+        HttpModule
+    ],
+    providers: [DashboardDataService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
