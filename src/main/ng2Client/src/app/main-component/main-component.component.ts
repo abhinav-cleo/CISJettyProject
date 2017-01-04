856aa8bee4e5d5f3583d96d6a3457ab0184db1a4
abhiit89 <abhi.aec89@gmail.com>
2017-01-04 11:55:55 +0530

    Navbar completed

--- src/main/ng2Client/src/app/main-component/main-component.component.ts
+++ src/main/ng2Client/src/app/main-component/main-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-main-component',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+  logout(){
+    this.router.navigate(['/login']);
+  }
 }
