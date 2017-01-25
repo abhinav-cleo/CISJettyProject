e27fd497ae3e3232b23e478e64fac83e5f6440f6
pdubey <pdubey@cleo.com>
2017-01-25 20:37:43 +0530

    Fetch AWS credentials from the credentials file

--- src/main/java/com/cleo/cis/server/AWS/AWSUtils.java
+++ src/main/java/com/cleo/cis/server/AWS/AWSUtils.java
@@ -1,8 +1,10 @@
 package com.cleo.cis.server.AWS;
 
 import com.amazonaws.AmazonClientException;
-import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.*;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 
+import java.io.File;
 /**
  * Created by rohit on 05/01/17.
  */
@@ -13,22 +15,27 @@ public class AWSUtils {
   public static AWSCredentials getCredentials() {
     AWSCredentials credentials = null;
     try {
-      credentials = new AWSCredentials() {
 
-        @Override
-        public String getAWSSecretKey() {
-          String aws_secret_key = System.getProperty("AWS_SECRET_KEY");
-          System.out.println("Secret key here: "+"b329IjEs3szUVxub2s/hdrpysKmpaANqQLSFbDsu");
-          return "b329IjEs3szUVxub2s/hdrpysKmpaANqQLSFbDsu";
-        }
-
-        @Override
-        public String getAWSAccessKeyId() {
-          System.out.println("access key here: "+"AKIAJ2QSLLMFPAKDLA4A");
-          return "AKIAJ2QSLLMFPAKDLA4A";
-                  //System.getProperty("AWS_ACCESS_KEY_ID");
-        }
-      };
+      //ProfileCredentialsProvider reads the credentials from ~/.aws/credentials location.
+      AWSCredentialsProvider awsCredentialsProvider = new ProfileCredentialsProvider();
+      credentials = awsCredentialsProvider.getCredentials();
+         // {
+//
+//        @Override
+//        public String getAWSSecretKey() {
+//          System.out.println(System.getProperties());
+//          String aws_secret_key = System.getProperty("AWS_SECRET_KEY");
+//          System.out.println("Secret key here: "+"b329IjEs3szUVxub2s/hdrpysKmpaANqQLSFbDsu");
+//          return "b329IjEs3szUVxub2s/hdrpysKmpaANqQLSFbDsu";
+//        }
+//
+//        @Override
+//        public String getAWSAccessKeyId() {
+//          System.out.println("access key here: "+"AKIAJ2QSLLMFPAKDLA4A");
+//          return "AKIAJ2QSLLMFPAKDLA4A";
+//                  //System.getProperty("AWS_ACCESS_KEY_ID");
+//        }
+//      };
     } catch (Exception e) {
       throw new AmazonClientException(
               "Cannot load the credentials from the credential profiles file. " +
